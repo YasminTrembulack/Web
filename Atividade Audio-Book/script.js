d4b76e13f58d0ebf25fa1e65a5f0acb71929eaa3
Yasmin <yas.trembulack@gmail.com>
2024-03-18 13:47:39 -0300

    Exercicio_Audio_JS

--- Atividade Audio-Book/script.js
+++ Atividade Audio-Book/script.js
@@ -1,12 +1,46 @@
 botao = document.getElementById("play-pause");
+audio = document.getElementById("audio-capitulo");
+texto_capitulo = document.getElementById("capitulo");
+botao_proximo = document.getElementById("proximo");
+botao_anterior = document.getElementById("anterior");
 
-let capitulo_js = 1;
+let capitulo = 1;
 
-console.log();
-// function playAudio{
-//     if(capitulo_js == 1){
+function playAudio(){
+    if(audio.paused){
+        audio.play();
+        botao.className = 'bi bi-pause-circle-fill';
+    }
+    else{
+        audio.pause();
+        botao.className = 'bi bi-play-circle-fill';
+    }
+}
 
-//     }
-// }
+function proximo(){
+    if(capitulo == 9){
+        capitulo = 1;
+    }
+    else{
+        capitulo++;
+    }
+    audio.setAttribute("src", `./books/dom-casmurro/${capitulo}.mp3`);
+    texto_capitulo.textContent = `Capítulo ${capitulo}`;
+    botao.className = 'bi bi-play-circle-fill';
+}
 
-// botao.
+function anterior(){
+    if(capitulo == 1){
+        capitulo = 9;
+    }
+    else{
+        capitulo--;
+    }
+    audio.setAttribute("src", `./books/dom-casmurro/${capitulo}.mp3`);
+    texto_capitulo.textContent = `Capítulo ${capitulo}`;
+    botao.className = 'bi bi-play-circle-fill';
+}
+
+botao.addEventListener('click', playAudio);
+botao_proximo.addEventListener('click', proximo);
+botao_anterior.addEventListener('click', anterior)
